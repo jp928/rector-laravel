bdec73d1d744d5a1c463c3f2b77d0463700e84f6
Jingtai Piao <jing.tai@ofload.com>
2025-09-03 08:52:20 +1000

    Lint fix

--- tests/config/configured_rule.php
+++ tests/config/configured_rule.php
@@ -2,8 +2,8 @@
 
 declare(strict_types=1);
 
-use RectorLaravelCustomRules\Rules\LaravelEloquentGenericRector;
 use Rector\Config\RectorConfig;
+use RectorLaravelCustomRules\Rules\LaravelEloquentGenericRector;
 
 return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->rule(LaravelEloquentGenericRector::class);
